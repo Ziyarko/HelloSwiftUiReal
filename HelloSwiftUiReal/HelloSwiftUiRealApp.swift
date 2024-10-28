d428afa7ddb3df23d7f9244c49baf61f6d521042
Ziyarko <ziya.kok@hisarschool.k12.tr>
2024-10-28 08:23:18 +0300

    Initial Commit

---
+++ HelloSwiftUiReal/HelloSwiftUiRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUiRealApp.swift
+//  HelloSwiftUiReal
+//
+//  Created by Ziya Kok on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUiRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloSwiftUiRealTests/HelloSwiftUiRealTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUiRealTests.swift
+//  HelloSwiftUiRealTests
+//
+//  Created by Ziya Kok on 28.10.2024.
+//
+
+import Testing
+@testable import HelloSwiftUiReal
+
+struct HelloSwiftUiRealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
